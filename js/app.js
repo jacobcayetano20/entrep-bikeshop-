316634cde0fc221b48aec8a1622b411fecbde672
Jacob Epino <42145719+jacobcayetano20@users.noreply.github.com>
2022-12-08 18:05:17 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     nav:true,
     items:1,
     dots:false,
-    smartSpeed:1000,
+    smartSpeed: 850,
     responsive:{
         0:{
             nav:false,
@@ -19,5 +19,45 @@ $(document).ready(function(){
 
 
 });
+
+
+var $grid = $('.collection-list').isotope({
+ 
+  });
+
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns(){
+    filterBtns.each(function(){
+      $(this).removeClass('active-filter-btn');
+    });
+  }
+//Product Details
+var ProductImg = document.getElementById("ProductImg");
+			var	SmallImg=document.getElementsByClassName("small-img");
+
+			SmallImg[0].onclick = function()
+			{
+				ProductImg.src = SmallImg[0].src;
+			}
+			SmallImg[1].onclick = function()
+			{
+				ProductImg.src = SmallImg[1].src;
+			}
+			SmallImg[2].onclick = function()
+			{
+				ProductImg.src = SmallImg[2].src;
+			}
+			SmallImg[3].onclick = function()
+			{
+				ProductImg.src = SmallImg[3].src;
+			}
+  
  
 
